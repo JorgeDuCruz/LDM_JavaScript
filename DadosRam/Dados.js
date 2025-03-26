b7e6fe74949c9382deb5bafd9504442e7c229b55
Jorge Durán Cruz <jdurancruz@danielcastelao.org>
2025-03-26 13:18:03 +0100

    Dados

---
+++ DadosRam/Dados.js
@@ -0,0 +1,33 @@
+let numDados = document.querySelector("#numDados")
+let bTirar = document.querySelector("#Tirar")
+let dados = document.querySelector("#dados")
+let Total = document.querySelector("#Total")
+
+bTirar.addEventListener("click",TirarDadoAuxiliar)
+
+function TirarDadoAuxiliar(){
+    console.log(numDados)
+    TirarDados(numDados.value)
+}
+
+function TirarDados(numDados){
+    console.log(2)
+    dados.innerHTML=""
+    let dado
+    let borrador
+    let total=0
+    for (i=0;i<numDados;i++){
+        dado = Math.floor(Math.random()*6)+1
+        total=total+dado
+        dado = "<img id = \"dado"+i+"\"src=\"Dice/diceRedAlt"+dado+".png\" style=\"width: 128px;height: 128px;margin: 5px;\">"
+        
+        setTimeout(function(){
+            dados.innerHTML = dados.innerHTML + "<img id=\"ani"+i+"\" src=\"Dice/SpinRedAlt.gif\">"
+            borrador = document.querySelector("#ani"+i)
+            borrador.style.display = "none"
+            console.log(borrador)
+        }) 
+        dados.innerHTML = dados.innerHTML + dado
+    }
+    Total.innerHTML = "<h1>Total = "+total+"</h1>"
+}
