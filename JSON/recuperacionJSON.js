9e647fcc4559e4c9eac608925476fa59c8fb0082
Jorge Duran Cruz <jdurancruz@danielcastelao.org>
2025-05-31 16:22:23 +0200

    Tarjeta terminada y funcional

--- JSON/recuperacionJSON.js
+++ JSON/recuperacionJSON.js
@@ -1,7 +1,18 @@
+//Variables para acceder a las distintas partes de la terjeta de resultado
 const generar = document.querySelector("#boton")
 generar.addEventListener("click",generarPokemon)
-let texto = document.querySelector("#pokemon")
+let nombrePokemon = document.querySelector("#pokemon-name")
+let fotoPokemon = document.querySelector("#pokemon-sprite")
+let generacionPokemon = document.querySelector("#pokemon-generation")
+let vidaPokemon = document.querySelector("#stat-hp")
+let ataquePokemon = document.querySelector("#stat-attack")
+let ataqueEsPokemon = document.querySelector("#stat-special-attack")
+let defensaPokemon = document.querySelector("#stat-defense")
+let defensaEsPokemon = document.querySelector("#stat-special-defense")
+let velocidadPokemon = document.querySelector("#stat-speed")
+let tiposPokemon = document.querySelector("#pokemon-types")
 
+//Variables para comprobar los botones de los tipos
 let normal = document.querySelector("#normal")
 let bug = document.querySelector("#bug")
 let ice = document.querySelector("#ice")
@@ -21,10 +32,9 @@ let psychic = document.querySelector("#psychic")
 let dragon = document.querySelector("#dragon")
 let electric = document.querySelector("#electric")
 
-let tipos = [normal,bug,ice,fairy,fighting,fire,flying,water,dark,dragon,ghost,grass,ground,rock,steel,poison,psychic,electric]
+let tipos = [normal,bug,ice,fairy,fighting,fire,flying,water,dark,dragon,ghost,grass,ground,rock,steel,poison,psychic,electric]//array con los botones de los distintos tipos 
 
-
-let tipoSeleccionado=""
+let tipoSeleccionado="" // variable para guardar que tipo fue seleccionado
 function generarPokemon(){
     //Comprobacion del tipo elegido
     for (let tipo of tipos){
@@ -33,7 +43,7 @@ function generarPokemon(){
         }
     }
 
-    console.log(tipoSeleccionado)
+    //console.log(tipoSeleccionado)
     //llamada a la API
     //fetch(`https://pokeapi.co/api/v2/type/${tipoSelecionado}`) no funciona con esta linea porque el formato no esta del todo bien, probablemente
     fetch('https://pokeapi.co/api/v2/type/'+tipoSeleccionado)
@@ -42,16 +52,87 @@ function generarPokemon(){
 }
 
 
-
+//Funcion para confirmar los datos del tipo elegido proporcionados por la API
 function respuestaCorrecta(respuesta){
-    respuesta.json().then(imprimeData)
+    respuesta.json().then(elegirPokemon)
 }
 
+//Funcion para llamar cuando una llamada a la API no se conecte
 function respuestaIncorrecta(){
     console.log("NO Funcione")
 }
 
-function imprimeData(respuesta){
-    texto.innerHTML=respuesta.pokemon[Math.floor(Math.random()*respuesta.pokemon.length)].pokemon.name
+let pokemon // variable para guardar el pokemon elegido
+function elegirPokemon(respuesta){
+    pokemon = respuesta.pokemon[Math.floor(Math.random()*respuesta.pokemon.length)] // elige un pokemon de la lista proporcionada por el tipo elegido
+    nombrePokemon.innerHTML=pokemon.pokemon.name //Escribe en la tarjeta el nombre de pokemon seleccionado
+    buscarPokemon(pokemon)//Buscar el resto de datos del pokemon
 }
 
+//Funcion que llama a la API para recibir los datos del pokemon elegido
+function buscarPokemon(pokemon){
+    fetch(pokemon.pokemon.url).then(confirmarPokemon).catch(respuestaIncorrecta)
+}
+
+//Funcion que confirma los datos del pokemon proporcionados por la API 
+function confirmarPokemon(respuesta){
+    respuesta.json().then(escribirPokemon)
+}
+
+//Funcion para escribir los datos del pokemon en la tarjeta del pokemon
+function escribirPokemon(pokemonRespuesta){
+    //console.log(pokemonRespuesta)
+    let stats = pokemonRespuesta.stats // Crea un Array con las estadisticas base del pokemon elegido
+
+    vidaPokemon.innerHTML = stats[0].base_stat //Escribe los puntos base de la estadistica de PS del pokemon
+    ataquePokemon.innerHTML = stats[1].base_stat //Escribe los puntos base de la estadistica de Ataque del pokemon
+    defensaPokemon.innerHTML = stats[2].base_stat //Escribe los puntos base de la estadistica de Defensa del pokemon
+    ataqueEsPokemon.innerHTML = stats[3].base_stat //Escribe los puntos base de la estadistica de Ataque especial del pokemon
+    defensaEsPokemon.innerHTML = stats[4].base_stat //Escribe los puntos base de la estadistica de Defensa especial del pokemon
+    velocidadPokemon.innerHTML = stats[5].base_stat //Escribe los puntos base de la estadistica de Velocidad del pokemon
+
+    fotoPokemon.src = pokemonRespuesta.sprites.front_default // Escribe el sprite del pokemon en la tarjeta
+    
+    tiposPokemon.innerHTML='' // vacia los tipos en caso de que haya habido un tipo antes
+    buscarTipos(pokemonRespuesta) // Busca los nombres en español de los tipos del pokemon
+
+    buscarGeneracion(pokemonRespuesta) // Busca la generacion en la que se introdujo la especie base del pokemon
+     // Esto hace que si el pokemon es una forma regional, megaEvolucion o similares establecera la generacion del pokemon original
+}
+
+// Funcion que llama a la API para recibir la informacion de los tipos del pokemon
+function buscarTipos(pokemon){
+    for (let tipo of pokemon.types){// For que recorre los tipos del pokemon en caso de que tenga mas de uno
+        fetch(tipo.type.url).then(confirmarTipo).catch(respuestaIncorrecta)//Llamda a la API
+    }
+}
+
+//Funcion para confirmar los datos del tipo proporcionados por la API
+function confirmarTipo(tipoRespuesta){
+    tipoRespuesta.json().then(escrbirTipos)
+}
+
+//Funcion que escribe los tipos del pokemon en español
+function escrbirTipos(tipo){
+    tiposPokemon.innerHTML += "<span class=\"pokemon-type\">"+tipo.names[5].name+"</span>"//Crea un span dentro de un div del html que tiene un estilo asociado en el css
+    //Tipo.names es un array que contiene objetos con los nombres del tipo elegido en diferentes idiomas y el objeto con indice 5 es el español
+    //El .name es la clave del objeto donde esta el nombre
+}
+
+//Funcion para llamar a la API de la especie de un pokemon para hallar su generacion
+function buscarGeneracion(pokemon){
+    fetch(pokemon.species.url).then(confirmarEspecie).catch(respuestaIncorrecta)
+}
+
+//Funcion que confirma los datos de la especie del pokemon proporcionados por la API
+function confirmarEspecie(especie){
+    especie.json().then(escribirGeneracion)
+}
+
+//Funcion que escribe en que generacion fue introducida la especie del pokemon
+function escribirGeneracion(especie){
+    let generacion = (especie.generation.name).toUpperCase().split("-")[1] /*Separa la informacion de la API en un Array que contien la palabra generation y
+                                                                            la generacion en la que la especie fue introducida con numeros romanos y en mayusculas
+                                                                            y finalmente elige la parte donde esta la generacion en numeros romanos*/
+    generacionPokemon.innerHTML = "Generación "+generacion //Escribe la generacion en la que fue introducida la especie del pokemon en la tarjeta del pokemon
+}
